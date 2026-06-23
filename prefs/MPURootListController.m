1366943c557730784174af5dc9f7eaf4fb6d6969
MurKaSkA00 <LerotaMer87@outlook.com>
2026-06-23 09:16:15 -0400

    Update MPURootListController.m

--- prefs/MPURootListController.m
+++ prefs/MPURootListController.m
@@ -2,12 +2,3 @@
 
 @interface MPURootListController : PSListController
 @end
-
-@implementation MPURootListController
-- (id)specifiers {
-    if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
-    }
-    return _specifiers;
-}
-@end
