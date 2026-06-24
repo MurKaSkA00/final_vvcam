0e45a4a418b7ea5be7b31f77ecc14fa94a11df2d
MurKaSkA00 <LerotaMer87@outlook.com>
2026-06-24 09:10:50 -0400

    Update MPURootListController.m

--- prefs/MPURootListController.m
+++ prefs/MPURootListController.m
@@ -1,5 +1,8 @@
-
 #import "MPURootListController.h"
+#import <notify.h>
+
+#define MPU_BUNDLE_ID  @"com.proximacore.mediaplaybackutils"
+#define MPU_NOTIF_NAME "com.proximacore.mpu/cfg"
 
 @implementation MPURootListController
 
@@ -13,6 +16,37 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.navigationItem.title = @"Media Playback Utilities";
+}
+
+// Зеркалим prefs из /var/jb/... в /var/mobile/... чтобы third-party sandbox их видел
+- (void)_mpu_mirrorPrefs {
+    NSArray *srcCandidates = @[
+        [NSString stringWithFormat:@"/var/jb/var/mobile/Library/Preferences/%@.plist", MPU_BUNDLE_ID],
+        [NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", MPU_BUNDLE_ID],
+    ];
+    NSString *dst = [NSString stringWithFormat:
+        @"/var/mobile/Library/Preferences/%@.plist", MPU_BUNDLE_ID];
+
+    NSData *data = nil;
+    for (NSString *src in srcCandidates) {
+        data = [NSData dataWithContentsOfFile:src];
+        if (data) break;
+    }
+    if (data) {
+        [data writeToFile:dst atomically:YES];
+        // делаем читаемым всем (на случай если umask порезал)
+        [[NSFileManager defaultManager] setAttributes:@{NSFilePosixPermissions: @0644}
+                                         ofItemAtPath:dst error:nil];
+    }
+    notify_post(MPU_NOTIF_NAME);
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+    [super setPreferenceValue:value specifier:specifier];
+    // дать cfprefsd флашнуть на диск
+    CFPreferencesSynchronize((__bridge CFStringRef)MPU_BUNDLE_ID,
+                             kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
+    [self _mpu_mirrorPrefs];
 }
 
 @end
